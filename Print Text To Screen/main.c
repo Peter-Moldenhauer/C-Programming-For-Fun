af3bfcee45e71307dee6642fba75f21429eda6df
Peter Moldenhauer <molde003@gmail.com>
2016-12-28 23:51:44 -0600

    Added first C programming fun file

---
+++ Print Text To Screen/main.c
@@ -0,0 +1,16 @@
+/*******************************************************************************
+** Name: Peter Moldenhauer
+** Date: 12/28/16
+** Description: This program demonstrates how you print text to the screen
+********************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello World from Peter Moldenhauer\n");  // command to print out text on the screen with escape character
+    printf("I am learning about C\n");
+
+    return 0;
+}
